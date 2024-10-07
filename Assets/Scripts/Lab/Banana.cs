ed4c7be9e1efab655ad31aca0bbdc857ee14e4f1
Kanoonkn9705 <pornwanach.peng@bumail.net>
2024-10-07 20:19:28 +0700

    Update Banana Script

--- Assets/Scripts/Lab/Banana.cs
+++ Assets/Scripts/Lab/Banana.cs
@@ -6,11 +6,6 @@ public class Banana : Weapon
 {
     [SerializeField] private float speed;
 
-    private void Start()
-    {
-        Move();
-    }
-
     public override void Move()
     {
         Debug.Log("Banana Move With Constant Speed");
